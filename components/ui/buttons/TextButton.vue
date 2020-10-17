c984a1417a1a34a36630b24ae440ab42b4c5525b
ReginaGimazova <reginagimazova98@gmail.com>
2020-10-17 13:49:49 +0300

    Add Tippy Component

---
+++ components/ui/buttons/TextButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="text-button">{{ this.text }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<style lang="scss">
+.text-button {
+  background: none;
+  color: $gray;
+  font-weight: 600;
+  padding: 10px;
+  border-radius: 3px;
+
+  &:hover {
+    background-color: $light-gray;
+  }
+}
+</style>
